5d6ff0a7fba82ce15f4eb11e7f2d7fbe0eb10640
prasu-2910 <prasuaruna29@gmail.com>
2025-12-16 14:41:00 +0530

    creating components

---
+++ day2question2/code/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div style={{ fontSize: "14px", color: "gray", padding: "10px", textAlign: "center" }}>
+      <h3>This is Footer</h3>
+    </div>
+  );
+}
+
+export default Footer;
